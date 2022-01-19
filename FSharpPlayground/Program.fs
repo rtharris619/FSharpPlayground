fe129244ad6b075abff2a53a4c77871f5c38bf41
Ryan Harris <ryan@servcraft.co.za>
2022-01-19 17:04:00 +0200

    first taste of F#

--- FSharpPlayground/Program.fs
+++ FSharpPlayground/Program.fs
@@ -1,4 +1,23 @@
 ﻿
+[1..100] |> List.sum |> printfn "sum=%d"
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let square x = x * x
+let sq = square 42
+printfn "Square 42? %A" sq
+
+type Person = {First:string; Last:string}
+
+type Employee =
+    | Worker of Person
+    | Manager of Employee list
+
+let jdoe = {First="John"; Last="Doe"}
+let worker = Worker jdoe
+
+let person1 = {First="Jane"; Last="Doe"}
+let person2 = {First="Tom"; Last="Doe"}
+printfn "Equal? %A" (person1.Last = person2.Last)
+
+let add2times3 = (+) 2 >> (*) 3
+let result = add2times3 5
+printfn "%A" result
