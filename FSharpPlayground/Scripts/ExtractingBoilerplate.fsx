a26fb4176150473503925b76e854577852629204
Ryan Harris <ryan@servcraft.co.za>
2022-02-03 13:18:20 +0200

    F# fun

---
+++ FSharpPlayground/Scripts/ExtractingBoilerplate.fsx
@@ -0,0 +1,26 @@
+﻿
+let product n =
+    let initialValue = 1
+    let action productSoFar x = productSoFar * x
+    [1..n] |> List.fold action initialValue
+
+
+let p = product 4
+printfn "%d" p
+
+let sumOfOdds n = 
+    let initialValue = 0
+    let action sumSoFar x = if x % 2 = 0 then sumSoFar else sumSoFar + x
+    [1..n] |> List.fold action initialValue
+
+let s = sumOfOdds 3
+printfn "%d" s
+
+let alternatingSum n =
+    let initialValue = (true, 0)
+    let action (isNeg, sumSoFar) x = if isNeg then (false, sumSoFar - x)
+                                              else (true, sumSoFar + x)
+    [1..n] |> List.fold action initialValue |> snd
+
+let a = alternatingSum(2)
+printfn "%d" a
---
+++ FSharpPlayground/Scripts/PatternMatching.fsx
@@ -0,0 +1,24 @@
+﻿
+type Shape = 
+    | Circle of int
+    | Rectangle of int * int
+    | Polygon of (int * int) list
+    | Point of (int * int)
+
+let draw shape =
+    match shape with
+    | Circle radius ->
+        printfn "The circle has a radius of %d" radius
+    | Rectangle (height, width) ->
+        printfn "The rectangle is %d high by %d wide" height width
+    | Polygon points ->
+        printfn "The polygon is made of these points %A" points
+    | _ -> printfn "I don't recognise this shape"
+
+
+let circle = Circle(10)
+let rectangle = Rectangle(4, 5)
+let polygon = Polygon([(1, 1); (2, 2); (3, 3)])
+let point = Point(2, 3)
+
+[circle; rectangle; polygon; point] |> List.iter draw
